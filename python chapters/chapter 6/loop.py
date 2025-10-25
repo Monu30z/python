5e355399aa543f3fa5ca3896889374013dc88395
Monu Singh <monusingh995606@gmail.com>
2025-10-25 22:08:14 +0530

    for and while loop in python

--- python chapters/chapter 6/loop.py
+++ python chapters/chapter 6/loop.py
@@ -1,5 +1,67 @@
-i = 1
+# i = 1
 
-while (i<=10) :
-    print(i)
-    i+=1
+# while (i<=10) :
+#     print(i)
+#     i+=1
+
+# i = 0
+# while(i<50):
+#     print("Monu Singh")
+#     i+=1
+
+
+# lists = [1,"Monu",False,"this","rohit","ajay"]
+
+# i = 0
+
+# while(i<len(lists)):
+#     print(lists[i])
+#     i+=1
+
+
+# for i in range(5):   # range(starting,ending,stepsize)
+#     print(i)
+
+# lists = [1,3,4,2,5,6]
+# for i in lists:
+#     print(i)
+
+# print("for loop in tuple")
+# tuples = (3,85,24,74,25,64)
+# for i in tuples:
+#     print(i)
+
+# s= "Monu"
+# for i in s:
+#     print(i)
+
+
+
+#?   for loop with else 
+
+# l = [1,6,4,2,68]
+# for item in l:
+#     print(item)
+
+# else:
+#     print("done")
+
+#?  break,continue and pass
+# for i in range(100):
+#     if (i==34):
+#         break
+#     print(i) 
+
+#?  break,continue and pass
+# for i in range(100):
+#     if (i==34):
+#         continue
+#     print(i) 
+
+
+
+#?  break,continue and pass
+for i in range(100):
+        pass
+   
+
---
+++ python chapters/chapter 6/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+l = [1,6,4,2,68]
+# for item in l:
+#     print(item)
+
+# else:
+#     print("done")
