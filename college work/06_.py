820bc7b186079c50d26129e0316f98f4911f7b03
Monu Singh <monusingh995606@gmail.com>
2025-11-06 11:12:42 +0530

    function with one or many return variable

--- college work/06_.py
+++ college work/06_.py
@@ -1,2 +1,53 @@
 
-#q :- What is function ? 
+# ? What is function ? 
+# :- a function is a block of code that perform a specific task 
+
+
+# def add():
+#     a,b = 10,20
+#     c=a+b
+#     print(c)
+
+# add()  
+
+
+# def add():
+#     x,y = 10,20
+#     z=x+y
+#     return z
+
+# print(add())
+
+
+# def add(y):
+#     x=10
+#     c= x+y
+#     d = x-y
+#     e= x*y
+#     return(c,d,e)
+
+# sum,sub,mult=add(5)
+# print(sum)
+# print(sub)
+# print(mult)
+
+# def disp():
+#     def show():
+#         print("show function")
+#     print("disp function")
+#     show()    
+
+
+# disp()
+
+
+def disp():
+    def show():
+        return "show function"
+    
+    result = show() + "disp function" 
+    return result
+  
+a= disp()
+print(a)
+
