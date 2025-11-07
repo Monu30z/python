58d3f10f86fb90c78be38d0f6b48231e37063e52
Monu Singh <monusingh995606@gmail.com>
2025-11-07 20:15:10 +0530

    Exception handling

---
+++ college work/ExceptionHeandling.py
@@ -0,0 +1,23 @@
+
+#? Exception ? 
+#*  An exception is a run time error which can be handle by Programmers 
+
+
+# try:
+#     print(x)
+# except:
+#     print("X not defind")    
+
+
+# a =10
+# b= 0
+# try:
+#     c = a/b
+#     print(c)
+# except ZeroDivisionError:
+#     print("division by zero not allowed")
+# else:
+#     print("else part : no exception in the program ")
+# finally:
+#     print("finally block ")   
+# print("rest of the code")
