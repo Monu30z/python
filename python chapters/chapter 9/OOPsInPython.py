ea0a7674571e9fc5f3b05859dbabbca50a0ebd21
Monu Singh <monusingh995606@gmail.com>
2025-11-10 15:58:47 +0530

    class methods and accessing them

--- python chapters/chapter 9/OOPsInPython.py
+++ python chapters/chapter 9/OOPsInPython.py
@@ -1,4 +1,3 @@
-z
 #?  class :- class is a blueprint for creating objects.
 
 # creating class
@@ -34,18 +33,75 @@ z
 
 
 
-class Students:
-    def __init__(self,name ,age):
-        self.name= name
-        self.age= age
+# class Students:
+#     def __init__(self,name ,age):
+#         self.name= name
+#         self.age= age
 
 
-    def show(self):
-        print(self.name)
-        print(self.age)
+#     def show(self):
+#         print(self.name)
+#         print(self.age)
       
 
 
-stu = Students("monu",34)
+# stu = Students("monu",34)
 
-stu.show()
+# stu.show()
+
+
+
+# class Moblie:
+#     name = "monu"            #? class variable  
+#     def __init__(self ):
+#         self.model = "realme"     #? instance variable  
+
+#     def Show_model(self,p):
+#         self.price = p   
+#         print(f"Model no : {self.model} price is : {self.price} ")
+
+# Mob = Moblie()
+# print(Mob.name)
+# Nokia = Moblie("nokia 3310")
+# Nokia.Show_model(2500)         
+# Oppo = Moblie("oppo reno6 5g")
+# Oppo.Show_model(25000)         
+# Maxx = Moblie("Maxx 07")
+# Maxx.Show_model(100000)         
+
+# realme = Moblie()
+# print(realme.model)        
+
+# print(id(Nokia))
+
+#? <============================================================================
+
+
+# class Mobile :
+#     fb = "yes"
+#     def __init__(self):
+#         self.model = "realme"
+#     def showModel(self):
+#         print(self.model)
+
+#     @classmethod   
+#     def show(cls):
+#         print(cls.fb)
+
+# realme = Mobile()
+# realme.showModel()
+# realme.show()
+
+
+class Mobile :
+    fb = "yes"
+    def __init__(self):
+        self.model = "realme"
+    def showModel(self):
+        print(self.model)
+
+   
+
+realme = Mobile()
+realme.showModel()
+print(Mobile.fb)         
